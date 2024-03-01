d37f00f05ed0c9a821cd274fbffc798325716256
thanhhai <nthanhhai219@gmail.com>
2024-03-01 08:35:45 +0700

    trangadmin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\Users\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NguoiDungController; //Tạo đường dẫn
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\MenuController;
+use PhpParser\Node\Expr\FuncCall;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +22,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/nguoidung', NguoiDungController::class);
+Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
+Route::middleware(['auth'])->group(function () {
+
+    #Menu
+
+    Route::prefix('admin')->group(function () {
+        Route::get('main', [MainController::class, 'index']);
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::prefix('menus')->group(function () {
+            Route::get('add ', [MenuController::class, 'create']);
+            Route::post('add ', [MenuController::class, 'store']);
+            Route::get('list', [MenuController::class, 'index']);
+            //Route::get('edit/{menu}', [MenuController::class, 'show']);
+            Route::match(['get', 'post'], 'edit/{menu}', [MenuController::class, 'show']);
+            Route::post('edit/{menu}', [MenuController::class, 'update']);
+            Route::DELETE('destroy', [MenuController::class, 'destroy']);
+        });
+
+        #Product    
+        Route::prefix('products')->group(function () {
+        });
+    });
+});
